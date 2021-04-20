c781143128c96517b1680fcb41c38caaaa1c8e33
dien_taufan <dien.taufan.lessy@gmail.com>
2021-04-20 11:03:09 +0700

    calculator

--- Main.java
+++ Main.java
@@ -9,11 +9,47 @@ public class Main
 {
     public static void main(String args[])
     {
-        Mahasiswa mahasiswa = new Mahasiswa();
-        System.out.println(mahasiswa.nama);
-        mahasiswa.nama = "mirza";
-        System.out.println(mahasiswa.nama);
-        System.out.println(mahasiswa.nama+" "+mahasiswa.membuatTugas("Matematika"));
+       System.out.println(Calculator.info);
+       
+       Calculator calculator1 = new Calculator();
+       Calculator calculator4 = new Calculator();
+       Calculator calculator2 = new Calculator(3,5);
+       Calculator calculator3 = new Calculator(3);
+       
+       System.out.println("Nilai awal setelah konstruktor dipanggil");
+       System.out.println("Nilai op2 Constructor tanpa parameter: "+calculator1.op2);
+       System.out.println("Nilai op2 Constructor dengan 2 parameter: "+calculator2.op2);
+       System.out.println("Nilai op2 Constructor dengan 3 parameter: "+calculator3.op2);
+       
+       System.out.println("Nilai op1 Constructor tanpa parameter: "+calculator1.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 2 parameter: "+calculator2.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 3 parameter: "+calculator3.getOp1());
+       
+       
+       System.out.println("Nilai setelah op1 dan op2 diganti");
+       calculator1.op2 = 10;
+       calculator2.op2 = 20;
+       calculator3.op2 = 30;
+       System.out.println("Nilai op2 Constructor tanpa parameter: "+calculator1.op2);
+       System.out.println("Nilai op2 Constructor dengan 2 parameter: "+calculator2.op2);
+       System.out.println("Nilai op2 Constructor dengan 3 parameter: "+calculator3.op2);
+       
+       calculator1.setOp1(40);
+       calculator2.setOp1(50);
+       calculator3.setOp1(60);
+       System.out.println("Nilai op1 Constructor tanpa parameter: "+calculator1.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 2 parameter: "+calculator2.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 3 parameter: "+calculator3.getOp1());
+        
+       System.out.println("");
+       System.out.println(Calculator.jumlahobject);
+       
+       System.out.println("");
+       double hasil1 = calculator1.jumlah();
+       double hasil2 = calculator1.jumlah(3.4, 5.4);
+       System.out.println("Hasil penjumlahan method jumlah tanpa parameter: "+ hasil1);
+       System.out.println("Hasil penjumlahan method jumlah dengan parameter: "+ hasil2);
+       
     }
     
 }
